f4fe9492a3894d9cf04ffc54f4c148f5500d30b0
Linda Marko <lindamarko.dev@gmail.com>
2022-09-09 20:22:41 +0200

    Add custom theme

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -41,6 +41,16 @@ module.exports = {
         ],
         dsl: 'less'
     },
+    magemasterycustom: {
+        area: 'frontend',
+        name: 'MageMastery/custom',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     backend: {
         area: 'adminhtml',
         name: 'Magento/backend',
